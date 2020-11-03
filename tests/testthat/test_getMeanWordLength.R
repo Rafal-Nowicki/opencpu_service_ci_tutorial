00703e823f03e563f4eff1d29d7d0f758e98f6b5
Rafal-Nowicki <rn68802@student.sgh.waw.pl>
2020-11-03 19:35:43 +0100

    Fixed test

--- tests/testthat/test_getMeanWordLength.R
+++ tests/testthat/test_getMeanWordLength.R
@@ -2,5 +2,5 @@ context("getMeanWordLength")
 
 test_that("Mean word length is computed correctly", {
     text = "Do you want the mustache on or off"
-    expect_equal(getMeanWordLength(text), 3.376)
+    expect_equal(getMeanWordLength(text), 3.375)
 })
